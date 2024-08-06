5a20382fc716c9ecc12edab880ff5cfd2d11e157
hye02eun <156975158+hye02eun@users.noreply.github.com>
2024-08-06 23:51:37 +0900

    [Bronze III] Title: 홀짝 칵테일, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/21312. 홀짝 칵테일/홀짝 칵테일.py
+++ 백준/Bronze/21312. 홀짝 칵테일/홀짝 칵테일.py
@@ -1,19 +1,18 @@
-box=list(map(int,input().split()))
+num = list(map(int, input().split()))
 
-#짝수 1개 라도 있는 경우 무조건 출력값은 짝수가 됨
-#홀수 3개일 때만 출력
+tmp = 1
+odd = [] # 홀수 저장
 
-odd=[]
-for i in range(3) : 
-    if (box[i] %2) != 0 : 
-        odd.append(box[i])
+# 홀수 판별
+for i in num:
+    if i % 2 != 0:
+        odd.append(i)
+    
+if odd:
+    for k in odd:
+        tmp *= k
+else:
+    for j in num:
+        tmp *= j
 
-#홀수가 없는 경우 짝수의 곱 중 가장 큰 곱
-    answer=1
-    if not odd : 
-        for i in range(3) : 
-            answer *= box[i]
-    else : 
-        for i in range(len(odd)) : 
-            answer *= odd[i]
-print(answer)
+print(tmp)
